492757bd069c02e7b91d33c40d6782f789c1e79f
Skip <skip@usecurling.com>
2025-11-21 22:37:20 +0000

    feat: Implement username edit limit on profile

---
+++ supabase/functions/_shared/hashing.ts
@@ -0,0 +1,7 @@
+export const hashAnswer = async (answer: string) => {
+  const msgUint8 = new TextEncoder().encode(answer.toLowerCase().trim())
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8)
+  const hashArray = Array.from(new Uint8Array(hashBuffer))
+  const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('')
+  return hashHex
+}
